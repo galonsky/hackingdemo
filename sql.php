a74be2fd1753129927ddab906424d593106a74e4
Alex Galonsky <alex@galonsky.com>
2012-03-13 14:45:33 -0400

    Resetting table on every page load

--- sql.php
+++ sql.php
@@ -1,9 +1,23 @@
 <?php
 
 //includes $host, $username, $password, $dbname
-include_once "db.php";
+require_once("db.php");
+
+function reset_table() {
+    $query = "DROP TABLE IF EXISTS `users`;";
+    mysql_query($query);
+    
+    $query = "CREATE TABLE IF NOT EXISTS `users` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `username` varchar(50) NOT NULL,
+      PRIMARY KEY (`id`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;";
+    
+    mysql_query($query);
+}
 
 $link = mysql_connect($host, $username, $password);
 mysql_select_db($dbname);
+reset_table();
 
 ?>
