919859da87b5323a84056547b16967dec3e6f860
Alex Galonsky <alex@galonsky.com>
2012-03-13 15:07:45 -0400

    Running query, working demo.

--- sql.php
+++ sql.php
@@ -1,3 +1,6 @@
+<html>
+<head><title>SQL Injection Demo</title></head>
+<body>
 <?php
 
 //includes $host, $username, $password, $dbname
@@ -12,12 +15,36 @@ function reset_table() {
       `username` varchar(50) NOT NULL,
       PRIMARY KEY (`id`)
     ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;";
+    mysql_query($query);
     
+    $query = "INSERT INTO `users` VALUES (null, 'alex');";
     mysql_query($query);
 }
 
-$link = mysql_connect($host, $username, $password);
-mysql_select_db($dbname);
-reset_table();
+if(isset($_GET['name'])) {
+    $name = $_GET['name'];
+    $link = mysql_connect($host, $username, $password);
+    mysql_select_db($dbname);
+    reset_table();
+    
+    $query = "SELECT * FROM users WHERE username = '" . $name . "';";
+    $result = mysql_query($query);
+    $num = mysql_num_rows($result);
+    if($num > 0) {
+        echo "<p>Authenticated sucessfully.</p>";
+    } else {
+        echo "<p>Incorrect credentials.</p>";
+    }
+}
+
 
 ?>
+<form action='sql.php' method='get'>
+    <label for='name'>Enter your name to enter the secure area:</label>
+    <input type='text' id='name' name='name'>
+    <input type='submit' value='submit'>
+</form>
+<p>Hint: here's the query that's being run:</p>
+<pre>$query = "SELECT * FROM users WHERE username = '" . $name . "';";</pre>
+</body>
+</html>
