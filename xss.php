84b2c656613f5585d94e052490588ab8d48bc584
Joe Levy <jal50@duke.edu>
2012-03-14 15:16:58 -0400

    some adjustments to xss page

--- xss.php
+++ xss.php
@@ -1,6 +1,3 @@
-<html>
-<head><title>Cross-site Scripting Demo</title></head>
-<body>
 <?php
 
 //includes $host, $username, $password, $dbname
@@ -10,13 +7,7 @@ function reset_table() {
     $query = "DROP TABLE IF EXISTS `comments`;";
     mysql_query($query);
     
-    $query = "CREATE TABLE IF NOT EXISTS `comments` (
-      `id` int(11) NOT NULL AUTO_INCREMENT,
-      `username` varchar(50) NOT NULL,
-      `comment` varchar(1024) NOT NULL,
-      PRIMARY KEY (`id`)
-    ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;";
-    mysql_query($query);
+    create_table();
     
     $query = "INSERT INTO `comments` VALUES (null, 'alex', 'i am alex, this is my comment');";
     mysql_query($query);
@@ -28,38 +19,54 @@ function reset_table() {
     mysql_query($query);
 }
 
+function create_table() {
+     $query = "CREATE TABLE IF NOT EXISTS `comments` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `username` varchar(50) NOT NULL,
+      `comment` varchar(1024) NOT NULL,
+      PRIMARY KEY (`id`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;";
+    mysql_query($query);
+}
+
 function showComments() {
     $query = "SELECT * FROM comments";
     $result = mysql_query($query);
 
     echo "<h2>Comments:</h2>";
     while ($row = mysql_fetch_assoc($result)) {
-        echo $row['name'] . " - " . $row['comment'] . "<br />";
+        echo $row['username'] . " - " . $row['comment'] . "<br /><br />";
     }
 }
 
-if(isset($_GET['name'])) {
-    $link = mysql_connect($host, $username, $password);
-    mysql_select_db($dbname);
-    
+$link = mysql_connect($host, $username, $password);
+mysql_select_db($dbname);
+create_table();
+
+if(isset($_POST['name'])) {    
     reset_table();
 
-    $query = "INSERT INTO `comments` VALUES (null, '" . $_GET['name'] . "', '" . $_GET['comment'] . "');";
+    $query = "INSERT INTO `comments` VALUES (null, '" . $_POST['name'] . "', '" . $_POST['comment'] . "');";
     mysql_query($query);
+
+    header('Location: xss.php');
+    return;
 }
-
-showComments();
-
 ?>
 
+<html>
+<head><title>Cross-site Scripting Demo</title></head>
+<body>
+<?php showComments(); ?>
 <br /><br />
 <h2>Enter a new comment:</h2>
-<form action='xss.php' method='get'>
+<form action='xss.php' method='post'>
     <label for='name'>Enter your name:</label>
     <br />
     <input type='text' id='name' name='name'>
     <br /><br />
-    <label for='name'>Enter your password:</label>&nbsp;&nbsp;<i>you can just enter any random password for the demo's purposes</i>
+    <label for='name'>Enter your password:</label>
+    <br /><i>you can just enter any random password for the demo's purposes</i>
     <br />
     <input type='text' id='password' name='password'>
     <br /><br />
